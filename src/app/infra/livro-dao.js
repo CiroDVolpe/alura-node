522128f8b077ccabf5b37e98049c5fca1aba737f
Ciro <cirodvolpe@gmail.com>
2020-07-14 17:49:57 -0300

    finalizado curso de node1 da alura.

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -5,7 +5,7 @@ class LivroDao {
 	}
 
 	lista() {
-		return new Promisse((resolve, reject) => {
+		return new Promise((resolve, reject) => {
 			this._db.all(
 				'SELECT * FROM livros',
 				(erro, resultados) => {
@@ -14,6 +14,97 @@ class LivroDao {
 					return resolve(resultados);
 				}
 			)
+		});
+	}
+
+	adiciona(livro) {
+		return new Promise((resolve, reject) => {
+			this._db.run(
+				`
+					INSERT INTO LIVROS (
+					titulo,
+					preco,
+					descricao
+					) values (?, ?, ?)
+				`,
+				[
+					livro.titulo,
+					livro.preco,
+					livro.descricao
+				],
+				function (erro) {
+					if (erro) {
+						console.log(erro);
+						return reject('Nao foi possivel adicionar o livro');
+					}
+
+					resolve();
+				}
+			)
+		});
+	}
+
+	buscaPorId(id) {
+		return new Promise((resolve, reject) => {
+			this._db.get(
+				`
+					SELECT *
+					FROM livros
+					WHERE id = ?
+				`,
+				[id],
+				(erro, livro) => {
+					if (erro) {
+						return reject('Não foi possível encontrar o livro!');
+					}
+					return resolve(livro);
+				}
+			);
+		});
+	}
+
+	atualiza(livro) {
+		return new Promise((resolve, reject) => {
+			this._db.run(
+			`
+				UPDATE livros SET
+				titulo = ?,
+				preco = ?,
+				descricao = ?
+				WHERE id = ?
+			`,
+			[
+				livro.titulo,
+				livro.preco,
+				livro.descricao,
+				livro.id
+			],
+			erro => {
+				if (erro) {
+					return reject('Não foi possível atualizar o livro!');
+				}
+
+				resolve();
+			});
+		});
+	}
+
+	remove(id) {
+		return new Promise((resolve, reject) => {
+			this._db.run(
+				`
+					DELETE 
+					FROM livros
+					WHERE id = ?
+				`,
+				[id],
+				(erro) => {
+					if (erro) {
+						return reject('Não foi possível remover o livro!');
+					}
+					return resolve();
+				}
+			);
 		});
 	}
 }
--- src/app/routes/rotas.js
+++ src/app/routes/rotas.js
@@ -17,7 +17,6 @@ module.exports = (app) => {
 	});
 
 	app.get(`/livros`, function(req, resp){
-
 		const livroDao = new LivroDao(db);
 		livroDao.lista().then(livros => resp.marko(
 			require('../views/livros/lista/lista.marko'),
@@ -25,6 +24,47 @@ module.exports = (app) => {
 				livros: livros
 			}
 		))
-		.catch(err0 => console.log(erro))
+		.catch(err0 => console.log(erro));
 	});
+
+	app.get(`/livros/form`, function(req, resp) {
+		resp.marko(require('../views/livros/form/form.marko'), { livro: [] })
+	});
+
+	app.post('/livros', function(req, resp) {
+		const livroDao = new LivroDao(db);
+		livroDao.adiciona(req.body)
+			.then(resp.redirect('/livros'))
+			.catch(error => console.log(error));
+	});
+
+	app.put('/livros', function(req, resp) {
+		const livroDao = new LivroDao(db);
+		livroDao.atualiza(req.body)
+			.then(resp.redirect('/livros'))
+			.catch(error => console.log(error));
+	});
+
+	app.delete('/livros/:id', function(req, resp) {
+    const id = req.params.id;
+    const livroDao = new LivroDao(db);
+    livroDao.remove(id)
+			.then(() => resp.status(200).end())
+			.catch(erro => console.log(erro));
+	});
+
+	app.get('/livro/form/:id', function(req, resp) {
+    const id = req.params.id;
+		const livroDao = new LivroDao(db);
+		
+    livroDao.buscaPorId(id)
+			.then(livro =>
+				resp.marko(
+					require('../views/livros/form/form.marko'),
+					{ livro: livro }
+				)
+			)
+			.catch(erro => console.log(erro));
+	});
+
 };
